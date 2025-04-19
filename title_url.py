ddeb8c35d9ea0c4ff283d6c5701ddc5b329de7ee
Lin Liqi <linliqi@mail.ustc.edu.cn>
2025-04-19 11:29:10 +0800

    Add files via upload
    get the title and url of a reference

---
+++ title_url.py
@@ -0,0 +1,91 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from serpapi import GoogleSearch
+import json
+
+class ReferenceInfoHandler:
+    def __init__(self, references):
+        self.references = references
+        self.article_titles = []  # 用于存储文章名称的列表
+        self.params = []  # 用于存储访问的列表
+        for ref in self.references:
+            if '文章名称' in ref:
+                self.article_titles.append(ref['文章名称'][0:50])
+        print(self.article_titles)
+        # self.ref_list_len = len(references)
+        # print(self.ref_list_len)
+        # self.keyword = self.references[0]['文章名称']
+        # print(self.keyword)
+        # 遍历 article_titles 列表，构建 params 列表
+        for title in self.article_titles:
+            # 构建字典，并将其添加到 params 列表
+            params_dict = {
+                'engine': 'google_scholar',
+                'q': title,  # 每个文章标题作为 q 的值
+                'api_key': '05769a47f0c9c04832b5669b540947598d4a703ba9460408335e88f7b28f8938'
+            }
+            self.params.append(params_dict)
+        self.result = {}
+
+        # 遍历 params 列表中的每个查询字典
+        for index, params_dict in enumerate(self.params):
+            # 创建 GoogleSearch 对象
+            search = GoogleSearch(params_dict)
+            
+            # 获取返回的字典数据
+            query_result = search.get_dict()
+            
+            # 将查询结果存储到 result 字典中，使用文献在 self.params 中的索引作为 key
+            self.result[index] = query_result
+
+        # 打印 result 字典，查看查询结果
+        # js = json.dumps(self.result, sort_keys=True, indent=4, separators=(',', ':'), ensure_ascii=False)
+        # print(js)
+
+        # 打印 params 列表中的每个字典
+        # print(self.params)
+        # self.params_test = {
+        #     'engine': 'google_scholar',
+        #     'q': 'X-detr: A versatile architecture for instance-wise',
+        #     'api_key': '05769a47f0c9c04832b5669b540947598d4a703ba9460408335e88f7b28f8938'
+        # }
+        # # 创建 GoogleSearch 对象
+        # search = GoogleSearch(self.params_test)
+        # # 获取返回的字典数据
+        # result = search.get_dict()
+        # query_url = result['search_metadata']['google_scholar_url']
+        # cited_num = result["organic_results"][0]["inline_links"]["cited_by"]["total"]
+        # print(cited_num)
+        # print(query_url)
+       
+
+    def search_title_and_url_on_google_scholar(self):
+        """
+        在 Google Scholar 上查询给定关键词的相关信息
+        :return: 查询结果的标题和链接
+        """
+        result_dict = {}
+        
+        try:
+            # 遍历 params 列表中的每个查询字典
+            for index, params_dict in enumerate(self.params):
+                # print(index)
+                query_result = {}
+                query_result["query_url"] = self.result[index]['search_metadata']['google_scholar_url']
+                query_result["title"] = self.result[index]["organic_results"][0]["title"]
+                # print(query_result)
+                result_dict[index] = query_result
+            # print(result_dict)
+            js = json.dumps(result_dict, sort_keys=True, indent=4, separators=(',', ':'), ensure_ascii=False)
+            print(js)
+
+        except requests.RequestException as e:
+            print(f"请求出错: {e}")
+            return None, None
+ref_list = [{'编号': '1', '作者': 'ZhaoweiCai,GukyeongKwon,AvinashRavichandran,ErhanBas,ZhuowenTu,RahulBhotika,andStefan0Soatto', 
+  '文章名称': 'X-detr: A versatile architecture for instance-wise visionlanguagetasks', '期刊': 'ArXiv,abs/2204'},
+   {'编号': '2', '作者': 'Ding-Jie Chen, Songhao Jia, Yi-Chen Lo, Hwann-Tzong Chen, and Tyng-Luh Liu', '文章名称': 'See-through-text'
+  ' grouping for referring image segmentation', '期刊': 'In Proceedings of the IEEE/CVF International Conference on Computer Vision (ICCV),2019'}] 
+handler = ReferenceInfoHandler(ref_list)
+handler.search_title_and_url_on_google_scholar()
